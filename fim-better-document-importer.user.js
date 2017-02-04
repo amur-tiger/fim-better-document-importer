e3277f88a0ffce9425a87ace5d549459065df170
Arne 'TigeR' Linck <a.linck@onlinehome.de>
2017-02-04 19:46:49 +0100

    Removed dependency on jQuery

--- fim-better-document-importer.user.js
+++ fim-better-document-importer.user.js
@@ -4,52 +4,108 @@
 // @version      0.1
 // @description  Adds a better importer for Google Docs documents to the chapter editor of FiMFiction.net.
 // @author       TigeR
-// @copyright    2016, TigeR
+// @copyright    2017, TigeR
 // @license      MIT, https://github.com/NekiCat/fim-better-document-importer/blob/master/LICENSE
 // @homepageURL  https://github.com/NekiCat/fim-better-document-importer
 // @match        *://www.fimfiction.net/chapter/*
-// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-(function($) {
+(function () {
     'use strict';
 
-    var logger = false;
-    var clientId = '285016570913-kin436digkbvboomjvnij5n9fitech9l.apps.googleusercontent.com';
+    var DO_LOGGING = true;
+    var CLIENT_ID = '285016570913-kin436digkbvboomjvnij5n9fitech9l.apps.googleusercontent.com';
+    var SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
     var oauthToken = null;
 
     // DOM objects and replacing the import button
-    var $editor = $('#chapter_editor');
-    var $oldbutton = $('#import_button');
+    var editor = document.getElementById('chapter_editor');
+    var oldButton = document.getElementById('import_button');
+    var button = oldButton.cloneNode(true);
 
-    var clone = $oldbutton[0].cloneNode(true);
-    $oldbutton[0].parentNode.replaceChild(clone, $oldbutton[0]);
-    var $button = $(clone);
+    oldButton.parentNode.replaceChild(button, oldButton);
 
-    // Pretty parsing function for the document
-    var fnParseDocument = function(doc) {
-        if (logger) console.log('Parsing document to BBCode.');
+    var Util = {
+        /**
+         * Loads a script dynamically by creating a script element and attaching it to the head element.
+         * @param {String} url
+         * @returns {Promise}
+         */
+        loadScript: function (url) {
+            return new Promise(function (resolve, reject) {
+                var script = document.createElement('script');
+                script.onload = resolve;
+                script.onerror = function () {
+                    console.error('Failed to load script: %s', url);
+                    reject.apply(this, arguments);
+                };
+                script.src = url;
+                document.getElementsByTagName('head')[0].appendChild(script);
+            });
+        },
 
-        // Utility function to parse "rgb(x, x, x)" strings from CSS
-        // Black is ignored, as that is the default
-        var rgb2hex = function(rgb) {
+        /**
+         * Loads a Google API dynamically.
+         * @param api
+         * @returns {Promise}
+         */
+        loadGoogleApi: function (api) {
+            return new Promise(function (resolve, reject) {
+                gapi.load(api, resolve);
+            });
+        },
+
+        getByAjax: function (url, options) {
+            return new Promise(function (resolve, reject) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == XMLHttpRequest.DONE) {
+                        if (xhr.status >= 200 && xhr.status <= 300) {
+                            resolve(xhr.response);
+                        } else {
+                            reject(xhr.response);
+                        }
+                    }
+                };
+                xhr.open('GET', url, true);
+                if (options && options.headers) {
+                    Array.from(options.headers).forEach((v, k) => xhr.setRequestHeader(k, v));
+                }
+                xhr.send();
+            });
+        },
+
+        /**
+         * Parses an RGB-color-string as returned from `element.style.color` to a CSS hex-notation.
+         * @param {String} rgb
+         * @returns {String|Boolean}
+         */
+        rgbToHex: function (rgb) {
             if (!rgb || rgb == 'inherit') return false;
-            rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-            var hex = function(x) {
-                return ("0" + parseInt(x).toString(16)).slice(-2);
+            var match = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+            var hex = function (x) {
+                return ('0' + parseInt(x).toString(16)).slice(-2);
             };
-            var c = '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+            var c = '#' + hex(match[1]) + hex(match[2]) + hex(match[3]);
             return c == '#000000' ? false : c;
-        };
+        },
 
-        // Utility function to convert PT as returned from Google to EM as required by FiMFiction
-        // 11pt and 12pt are ignored as these are the defaults
-        var ptToEm = function(pt) {
+        /**
+         * Converts a font size in PT to a font size in EM, assuming default values for DPI...
+         * @param {String} pt
+         * @returns {String|Boolean}
+         */
+        ptToEm: function (pt) {
             if (!pt) return false;
             pt = pt.slice(0, -2);
             if (pt == '11' || pt == '12') return false;
             return +(pt / 12).toFixed(3) + 'em';
-        };
+        }
+    };
+
+    // Pretty parsing function for the document
+    var fnParseDocument = function (doc) {
+        if (DO_LOGGING) console.log('Parsing document to BBCode.');
 
         // Walk a paragraph for all styles recursively
         // Google doesn't report styles recursively, so this might be overkill
@@ -60,9 +116,9 @@
                 // Links get special treatment since the color and underline by Google
                 // can be ignored, the default styling is used instead
                 // Also, strip the Google referrer link out
-                var atemp = document.createElement('a');
-                atemp.href = this.children[0].getAttribute('href');
-                var queryParams = atemp.search.substring(1).split('&');
+                var tmpA = document.createElement('a');
+                tmpA.href = this.children[0].getAttribute('href');
+                var queryParams = tmpA.search.substring(1).split('&');
                 var link;
                 for (var i = 0; i < queryParams.length; i++) {
                     var pair = queryParams[i].split('=');
@@ -77,7 +133,7 @@
                     // structure, formatted links might get ripped into multiple pieces...
                     return '[url=' + link + ']' + fnWalk.call(this.children[0]) + '[/url]';
                 } else {
-                    console.error('Failed to parse Google referral URL: %s', atemp.href);
+                    console.error('Failed to parse Google referral URL: %s', tmpA.href);
                 }
             }
             if (this.children.length == 1 && this.children[0].nodeName == 'IMG') {
@@ -95,8 +151,8 @@
                 italic = this.style.fontStyle == 'italic';
                 underline = this.style.textDecoration == 'underline';
                 strike = this.style.textDecoration == 'line-through';
-                color = rgb2hex(this.style.color);
-                size = ptToEm(this.style.fontSize);
+                color = Util.rgbToHex(this.style.color);
+                size = Util.ptToEm(this.style.fontSize);
 
                 if (bold) text += '[b]';
                 if (italic) text += '[i]';
@@ -106,7 +162,7 @@
                 if (size) text += '[size=' + size + ']';
             }
 
-            Array.from(this.childNodes).forEach(function(e) {
+            Array.from(this.childNodes).forEach(function (e) {
                 text += walk.call(e);
             });
 
@@ -122,11 +178,12 @@
             return text;
         };
 
-        var $doc = $(doc);
         var contents = '';
+        var template = document.createElement('template');
+        template.innerHTML = doc;
 
         // Walk all elements in the document
-        $doc.each(function() {
+        template.contents.forEach(function () {
             if (this.nodeName === 'P') {
                 var ptext = fnWalk.call(this);
 
@@ -140,64 +197,58 @@
             }
         });
 
-        $editor.val(contents);
+        editor.value = contents;
     };
 
     // Loading the Google API scripts
-    $.getScript('https://apis.google.com/js/api.js', function() {
-        gapi.load('picker:auth', {
-            callback: function() {
-                if (logger) console.log('Authorizing with Google.');
-                gapi.auth.authorize({
-                    client_id: clientId,
-                    scope: ['https://www.googleapis.com/auth/drive.readonly'],
-                    immediate: true,
-                }, function(result) {
-                    if (result) {
-                        if (logger) console.log('Got authorization from Google.');
-                        oauthToken = result.access_token;
-                    } else {
-                        console.error('Error getting authorization: %o', result);
-                        ShowErrorWindow('Sorry! There was an error authenticating you with Google. Only the standard import mechanism will be available.');
-                        $button[0].parentNode.replaceChild($oldbutton[0], $button[0]);
-                    }
-                });
-            },
+    Util.loadScript('https://apis.google.com/js/api.js')
+        .then(() => Util.loadGoogleApi('picker:auth'))
+        .then(function () {
+            if (DO_LOGGING) console.log('Authorizing with Google.');
+            gapi.auth.authorize({
+                client_id: CLIENT_ID,
+                scope: SCOPES,
+                immediate: true
+            }, function (result) {
+                if (result) {
+                    if (DO_LOGGING) console.log('Got authorization from Google.');
+                    oauthToken = result.access_token;
+                } else {
+                    console.error('Error getting authorization: %o', result);
+                    ShowErrorWindow('Sorry! There was an error authenticating you with Google. Only the standard import mechanism will be available.');
+                    button.parentNode.replaceChild(oldButton, button);
+                }
+            });
         });
-    });
 
     // Create and show picker and retrieve document
-    $button.click(function(e) {
+    button.addEventListener('click', function (e) {
         var picker = new google.picker.PickerBuilder()
-        .setOAuthToken(oauthToken)
-        .setAppId(clientId)
-        .addView(google.picker.ViewId.RECENTLY_PICKED)
-        .addView(google.picker.ViewId.DOCUMENTS)
-        .setCallback(function(data) {
-            if (data.action != 'picked') return;
+            .setOAuthToken(oauthToken)
+            .setAppId(CLIENT_ID)
+            .addView(google.picker.ViewId.RECENTLY_PICKED)
+            .addView(google.picker.ViewId.DOCUMENTS)
+            .setCallback(function (data) {
+                if (data.action != 'picked') return;
 
-            var doc = data.docs[0];
-            if (doc.mimeType != 'application/vnd.google-apps.document') {
-                ShowErrorWindow('Sorry! Only Google documents can be imported as of now.');
-                return;
-            }
+                var doc = data.docs[0];
+                if (doc.mimeType != 'application/vnd.google-apps.document') {
+                    ShowErrorWindow('Sorry! Only Google documents can be imported as of now.');
+                    return;
+                }
 
-            if (logger) console.log('Importing document ' + doc.name + '.');
+                if (DO_LOGGING) console.log('Importing document ' + doc.name + '.');
 
-            $.ajax({
-                type: 'GET',
-                url: 'https://www.googleapis.com/drive/v3/files/' + doc.id + '/export?mimeType=text/html',
-                headers: {
-                    Authorization: 'Bearer ' + oauthToken,
-                },
-                success: fnParseDocument,
-                error: function(data) {
-                    console.error('Error getting document: %o', data);
+                Util.getByAjax('https://www.googleapis.com/drive/v3/files/' + doc.id + '/export?mimeType=text/html', {
+                    headers: {
+                        Authorization: 'Bearer ' + oauthToken
+                    }
+                }).then(fnParseDocument, function (response) {
+                    console.error('Error getting document: %o', response);
                     ShowErrorWindow('Sorry! There was an error retrieving the document from Google.');
-                },
-            });
-        })
-        .build();
+                });
+            })
+            .build();
         picker.setVisible(true);
     });
-})(jQuery);
+})();
