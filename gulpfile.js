ea7f3bda69ea45d40da3f8d66cadb8ecf2d4a55c
Arne 'TigeR' Linck <a.linck@onlinehome.de>
2017-03-18 17:49:33 +0100

    Removed IDE-specific settings in favor of editorconfig

--- gulpfile.js
+++ gulpfile.js
@@ -10,34 +10,34 @@ const pkg = require("./package.json");
 const external = Object.keys(pkg.dependencies || {});
 
 gulp.task("build", () => {
-    "use strict";
+	"use strict";
 
-    merge(
-        gulp.src("./header.txt")
-            .pipe(replace(/\$\{version}/g, pkg.version))
-            .pipe(replace(/\$\{description}/g, pkg.description))
-            .pipe(replace(/\$\{author}/g, pkg.author))
-            .pipe(replace(/\$\{homepage}/g, pkg.homepage))
-            .pipe(replace(/\$\{bugs}/g, pkg.bugs.url)),
+	merge(
+		gulp.src("./header.txt")
+			.pipe(replace(/\$\{version}/g, pkg.version))
+			.pipe(replace(/\$\{description}/g, pkg.description))
+			.pipe(replace(/\$\{author}/g, pkg.author))
+			.pipe(replace(/\$\{homepage}/g, pkg.homepage))
+			.pipe(replace(/\$\{bugs}/g, pkg.bugs.url)),
 
-        gulp.src("./src/**/*.ts")
-            .pipe(rollup({
-                entry: "src/main.ts",
-                plugins: [
-                    typescript()
-                ],
-                external: external,
-                format: "iife"
-            }))
-    )
-        .pipe(concat(pkg.main))
-        .pipe(gulp.dest("."));
+		gulp.src("./src/**/*.ts")
+			.pipe(rollup({
+				entry: "src/main.ts",
+				plugins: [
+					typescript()
+				],
+				external: external,
+				format: "iife"
+			}))
+	)
+		.pipe(concat(pkg.main))
+		.pipe(gulp.dest("."));
 });
 
 gulp.task("watch", () => {
-    "use strict";
+	"use strict";
 
-    gulp.watch("./src/**/*.ts", ["build"]);
-    gulp.watch("./header.txt", ["build"]);
-    gulp.watch("./package.json", ["build"]);
+	gulp.watch("./src/**/*.ts", ["build"]);
+	gulp.watch("./header.txt", ["build"]);
+	gulp.watch("./package.json", ["build"]);
 });
